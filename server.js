d8c834c628dbfa07852b08503cb1f87f9c6f847d
Andres Urdaneta <aeum3893@gmail.com>
2019-11-02 15:53:29 -0400

    created dbRoute with the path for the connection with the mongo db cluster

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParse = require('body-parser')
+
+const dbRoute =
+    'mongodb+srv://andresurdaneta:<password>@techtoprotect-g3o5b.mongodb.net/test?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, console.log(`Running in port ${PORT}`))
