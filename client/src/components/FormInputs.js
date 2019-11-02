421c677fa72a1ddf4fedb7e669df8395b6d67312
James Kemp <jameskemp2796@yahoo.com>
2019-11-02 16:27:33 -0400

    finished form inputs

--- client/src/components/FormInputs.js
+++ client/src/components/FormInputs.js
@@ -1,22 +1,23 @@
 import React from 'react'
 
-const FormInputs = () => {
-	return(
-		<div id="form-group">
-			<form>
-				<label htmlFor="name-input">Name: </label>
-				<input type="text" />
-				<label htmlFor="gender-input">Gender: </label>
-				<input type="text" />
-				<label htmlFor="age-input">Age: </label>
-				<input type="text" />
-				<label htmlFor="condition-input">Condition: </label>
-				<input type="text" />
-				<label htmlFor="name-input">Name: </label>
-				<input type="text" />
-				<label htmlFor="name-input">Name: </label>
-				<input type="text" />
-			</form>
-		</div>
-	)
-}
+const FormInputs = () => (
+	<div id="form-group">
+		<form id="form-inputs">
+			<label htmlFor="name-input">Name: </label>
+			<input type="text" />
+			<label htmlFor="gender-input">Gender: </label>
+			<input type="text" />
+			<label htmlFor="age-input">Age: </label>
+			<input type="text" />
+			<label htmlFor="condition-input">Condition: </label>
+			<input type="text" />
+			<label htmlFor="name-input">Name: </label>
+			<input type="text" />
+			<label htmlFor="name-input">Name: </label>
+			<input type="text" />
+			<button type="submit">SUBMIT</button>
+		</form>
+	</div>
+)
+
+export default FormInputs
--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import FormInputs from '../components/FormInputs'
 
 export class Form extends React.Component {
 
   render(){
 		return(
-			<h1>Hello World</h1>
+			<>
+				<h1>Form</h1>
+				<FormInputs />
+			</>		
 		)
 	}
 }
