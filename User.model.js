5e9935f1c5c87a9a820996a1ea5d2f49dcc1d943
Andres Urdaneta <aeum3893@gmail.com>
2019-11-03 17:57:09 -0500

    developed with more detail the database schema

--- User.model.js
+++ User.model.js
@@ -6,7 +6,18 @@ const { Schema } = mongoose
 const userSchema = new Schema(
     {
         id: { type: Number },
-        status: { type: String }
+        status: { type: String },
+        personalInfo: {
+            name: { type: String },
+            age: { type: Number },
+            address: { type: String },
+            phone: { type: Number }
+        },
+        medicalInfo: {
+            respirations: { type: Boolean },
+            perfusion: { type: Number },
+            mentalStatus: { type: Boolean }
+        }
     }
 )
 
