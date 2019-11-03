7a08dc0306530584a6e38342c7cfa02027819f5b
Andres Urdaneta <aeum3893@gmail.com>
2019-11-02 23:00:29 -0400

    method to fetch all data in the database, and create method

--- server.js
+++ server.js
@@ -1,10 +1,49 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const bodyParse = require('body-parser')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+let cors = require('cors')
+const router = express.Router()
 
-const dbRoute =
-    'mongodb+srv://andresurdaneta:55865656@techtoprotect-g3o5b.mongodb.net/test?retryWrites=true&w=majority'
+const Data = require('./data')
+
+app.use(cors())
+
+const dbRoute = 'mongodb+srv://andresurdaneta:55865656@techtoprotect-g3o5b.mongodb.net/test?retryWrites=true&w=majority'
+
+//connect with the database
+mongoose.connect(dbRoute, { useNewUrlParser: true, useUnifiedTopology: true })
+
+let db = mongoose.connection
+
+db.once('open', () => console.log('connected to the database'))
+
+// checks if connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+// this is our get method
+// this method fetches all available data in our database
+router.get('/getData', (req, res) => {
+    Data.find((err, data) => {
+        if (err) return `this is the error ${err}`
+        return res.json({ data })
+    })
+})
+
+router.post('/putData', (req, res) => {
+    let data = new Data()
+
+    const { id } = req.body;
+
+    data.id = id;
+    data.save((err) => {
+        if (err) return `This is the error ${err}`
+        return res.json({ data })
+    })
+})
+
+app.use('/api', router);
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, console.log(`Running in port ${PORT}`))
